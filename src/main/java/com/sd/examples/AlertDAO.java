46a75dc3d6788a29a555b11a633a53df77e53c55
sibendu_das <sibendu.das@oracle.com>
2019-09-12 12:28:33 +0530

    t

--- src/main/java/com/sd/examples/AlertDAO.java
+++ src/main/java/com/sd/examples/AlertDAO.java
@@ -1,14 +1,52 @@
-package com.sd.examples;   
+package com.sd.examples; 
 
-import java.sql.*;
+import java.sql.*;  
 
-class AlertDAO {
+class AlertDAO { 
+	 
+	private String url = null;
+	private String user = null;
+	private String password = null;
 	
-	public static void main(String args[]) { 
+	public static void main(String args[]) {
+		 
+		AlertDAO dao = new AlertDAO("localhost:3306/kafka", "root", "password");
+		
+		dao.processAlert(type, Date created_date, String account, Integer no_access, String details,
+				String status);
+		
+		System.out.println("Alert created .. ");
+		
+		dao.findAlerts();
+		System.out.println("Alerts displayed .. ");
+		
+	}
+	
+	public AlertDAO(String url, String user, String password) {
+		this.url = url;
+		this.user = user;
+		this.password = password;
+	}
+	
+	public void processAlert(String type, Date created_date, String account, Integer no_access, String details,
+			String status) throws Exception {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://"+url,user, password);
+		Statement stmt = con.createStatement();
+
+		String query = "insert into account_alerts (type,created_date,account,no_access,details,status) " + " VALUES ('"
+				+ type + "', CURDATE() , '" + account + "' , " + no_access + " , '" + details + "' ,'" + status + "') ";
+
+		ResultSet rs = stmt.execute(query);
+		con.close();
+	}
+
+	public void findAlerts() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/kafka", "root", "password");
 			Statement stmt = con.createStatement();
+
 			ResultSet rs = stmt.executeQuery("select * from account_alerts");
 			while (rs.next())
 				System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
